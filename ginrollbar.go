a04b26ca3693b7c7d8502d5e0f1517cf636058ba
Sean Lane <sean.lane@neighbor.com>
2022-09-13 11:17:12 -0600

    Use pkg/errors, add helper functions for rollbar

--- ginrollbar.go
+++ ginrollbar.go
@@ -1,13 +1,13 @@
 package ginrollbar
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"runtime"
 	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"github.com/rollbar/rollbar-go"
 )
 
@@ -45,4 +45,42 @@ func getCallers(skip int) (pc []uintptr) {
 	pc = make([]uintptr, 1000)
 	i := runtime.Callers(skip+1, pc)
 	return pc[0:i]
+}
+
+// Helper functions to convert pkg/errors stack traces to be compatible with rollbar
+// Cribbed from pkg/errors
+
+// pc returns the program counter for this frame;
+// multiple frames may have the same PC value.
+func FramePc(f errors.Frame) uintptr { return uintptr(f) - 1 }
+
+// file returns the full path to the file that contains the
+// function for this Frame's pc.
+func FrameFile(f errors.Frame) string {
+	fn := runtime.FuncForPC(FramePc(f))
+	if fn == nil {
+		return "unknown"
+	}
+	file, _ := fn.FileLine(FramePc(f))
+	return file
+}
+
+// line returns the line number of source code of the
+// function for this Frame's pc.
+func FrameLine(f errors.Frame) int {
+	fn := runtime.FuncForPC(FramePc(f))
+	if fn == nil {
+		return 0
+	}
+	_, line := fn.FileLine(FramePc(f))
+	return line
+}
+
+// name returns the name of this function, if known.
+func FrameName(f errors.Frame) string {
+	fn := runtime.FuncForPC(FramePc(f))
+	if fn == nil {
+		return "unknown"
+	}
+	return fn.Name()
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.8.0
+	github.com/pkg/errors v0.9.1
 	github.com/rollbar/rollbar-go v1.4.2
 	github.com/stretchr/testify v1.7.1
 )
